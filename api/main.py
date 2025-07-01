9f0aaae979e0e8f5692c284e3a47827a75de7d92
aungkohtat <aungkohtet717@gmail.com>
2025-07-01 14:07:28 +0700

    bookstore

--- api/main.py
+++ api/main.py
@@ -277,4 +277,4 @@ def checkout():
     return jsonify({"success": True})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
